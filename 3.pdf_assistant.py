abab42601843f395c66e00956990c3e908c2f332
Harsha Mahadev <harshamahadev13@gmail.com>
2025-03-08 19:02:34 +0530

    new

--- 3.pdf_assistant.py
+++ 3.pdf_assistant.py
@@ -1,6 +1,21 @@
+# RAG application.
+# Using vectorDB to store information contained in a PDF which is in a URL. 
+# Retrieve the appropriate answers from the VectorDB using the Agent.
+
+
 # There are 2 ways to run the Vector DB. 
 # 1) via docker - local.  2) via AWS, AZURE, etc
 # In this we will use docker file to read a PDF document.
+
+
+# VectorDB
+# Converting & Storing & Using 
+# Create a Vector DB in Docker. 
+# The content from the pdf will be extracted and stored in the Vector DB
+# That content will be used to give accurate results based on the input search.
+
+# Step-1
+# Create the VectorDB using docker
 
 
 
--- readme.txt
+++ readme.txt
@@ -1,5 +1,8 @@
-Ordered in Increasing complexity
+Ordered in Increasing Complexity
 
-Standard code           --> app.py
-Chatbot type code       --> playground.py
+1) app.py               --> Standard code.
+2) playground.py        --> phidata chatbot with Standard code. 
+3) pdf_assistant        --> reading pdf using RAG.
 
+
+PHIDATA documentation is the key for understanding and implementing new concepts.

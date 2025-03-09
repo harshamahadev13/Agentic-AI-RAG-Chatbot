6e5eea8544960e3001f414629a55c2abc7f8a924
Harsha Mahadev <harshamahadev13@gmail.com>
2025-03-09 09:48:58 +0530

    new

--- pdf_assistant_3.py
+++ pdf_assistant_3.py
@@ -57,4 +57,4 @@ def pdf_assistant(new: bool = False, user: str = "user"):
     assistant.cli_app(markdown=True)
 
 if __name__=="__main__":
-    typer.run(pdf.assistant) 
+    typer.run(pdf_assistant) 
